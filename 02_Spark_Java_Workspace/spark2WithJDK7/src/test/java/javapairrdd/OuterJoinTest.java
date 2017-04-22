c064dd476743e6d22296cd308f682f85b3930212
wooheaven <wooheaven79@gmail.com>
2017-04-23 02:38:16 +0900

    #3 add right outer join

--- 02_Spark_Java_Workspace/spark2WithJDK7/src/test/java/javapairrdd/OuterJoinTest.java
+++ 02_Spark_Java_Workspace/spark2WithJDK7/src/test/java/javapairrdd/OuterJoinTest.java
@@ -13,9 +13,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LeftOuterJoinTest implements Serializable {
+public class OuterJoinTest implements Serializable {
     @Test
-    public void testLeftOuterJoin() {
+    public void testOuterJoin() {
         JavaSparkContext sc = new JavaSparkContext(new SparkConf().setMaster("local[*]").setAppName("LeftOuterJoin"));
         List<Tuple2<String, Integer>> left = new ArrayList<Tuple2<String, Integer>>();
         left.add(new Tuple2<String, Integer>("A", 2));
@@ -28,21 +28,38 @@ public class LeftOuterJoinTest implements Serializable {
         right.add(new Tuple2<String, Integer>("B", 3));
         right.add(new Tuple2<String, Integer>("C", 3));
         right.add(new Tuple2<String, Integer>("D", 3));
+        right.add(new Tuple2<String, Integer>("F", 3));
 
         JavaPairRDD<String, Integer> leftRdd = sc.parallelizePairs(left);
         JavaPairRDD<String, Integer> rightRdd = sc.parallelizePairs(right);
 
-        JavaPairRDD<String, Tuple2<Integer, Optional<Integer>>> joinSample = leftRdd.leftOuterJoin(rightRdd);
-        JavaPairRDD<String, Integer> result = joinSample.mapValues(new Function<Tuple2<Integer, Optional<Integer>>, Integer>() {
+        // left outer join
+        JavaPairRDD<String, Tuple2<Integer, Optional<Integer>>> leftJoin = leftRdd.leftOuterJoin(rightRdd);
+        JavaPairRDD<String, Integer> leftJoinResult = leftJoin.mapValues(new Function<Tuple2<Integer, Optional<Integer>>, Integer>() {
             @Override
             public Integer call(Tuple2<Integer, Optional<Integer>> v1) throws Exception {
                 return v1._2().or(v1._1());
             }
         });
-        result.foreach(new VoidFunction<Tuple2<String, Integer>>() {
+        leftJoinResult.foreach(new VoidFunction<Tuple2<String, Integer>>() {
             @Override
             public void call(Tuple2<String, Integer> t) throws Exception {
                 System.out.println(t._1() + " " + t._2());
+            }
+        });
+
+        // right outer join
+        JavaPairRDD<String, Tuple2<Optional<Integer>, Integer>> rightJoin = leftRdd.rightOuterJoin(rightRdd);
+        JavaPairRDD<String, Integer> rightJoinResult = rightJoin.mapValues(new Function<Tuple2<Optional<Integer>, Integer>, Integer>() {
+            @Override
+            public Integer call(Tuple2<Optional<Integer>, Integer> v) throws Exception {
+                return v._1().or(v._2());
+            }
+        });
+        rightJoinResult.foreach(new VoidFunction<Tuple2<String, Integer>>() {
+            @Override
+            public void call(Tuple2<String, Integer> t) throws Exception {
+                System.out.println(t._1 + " " + t._2());
             }
         });
     }
