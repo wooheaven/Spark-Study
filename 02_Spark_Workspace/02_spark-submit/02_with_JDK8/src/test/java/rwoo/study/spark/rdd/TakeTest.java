bc2f73040461ab5bb1266a6cf0c03a0e221aca29
ubuntu <wooheaven79@gmail.com>
2019-12-31 13:36:08 +0900

    #178-javardd-take ``` new file:   02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/rdd/TakeTest.java modified:   README.md ```

---
+++ 02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/rdd/TakeTest.java
@@ -0,0 +1,36 @@
+package rwoo.study.spark.rdd;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TakeTest {
+    private JavaSparkContext sc;
+    private JavaRDD<Integer> rddA;
+    private List<Integer> result;
+
+    @Before
+    public void setUp() {
+        sc = new JavaSparkContext("local", "JavaRDD.take");
+        rddA = sc.parallelize(Arrays.asList(1, 2, 3, 2, 3, 10));
+    }
+
+    @After
+    public void after() {
+        assertEquals(Arrays.asList(1, 2, 3, 2, 3, 10), rddA.collect());
+        sc.close();
+    }
+
+    @Test
+    public void testTake() {
+        result = rddA.take(4);
+        assertEquals(Arrays.asList(1, 2, 3, 2), result);
+    }
+}
