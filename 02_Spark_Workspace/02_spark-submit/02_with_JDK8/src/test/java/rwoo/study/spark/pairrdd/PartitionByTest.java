70708edd23fb9ea3d6bd86d0a6128fb949beb307
ubuntu <wooheaven79@gmail.com>
2020-01-20 02:05:07 +0900

    #191-javapairrdd-partitionby ``` modified:   02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/main/java/rwoo/study/spark/example/PageRank.java deleted:    02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/main/java/rwoo/study/spark/partitioner/CustomPartitioner.java modified:   02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/pairrdd/PartitionByTest.java modified:   README.md ```

--- 02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/pairrdd/PartitionByTest.java
+++ 02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/pairrdd/PartitionByTest.java
@@ -1,73 +1,119 @@
 package rwoo.study.spark.pairrdd;
 
 import org.apache.spark.HashPartitioner;
-import org.apache.spark.SparkConf;
+import org.apache.spark.Partitioner;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import rwoo.study.spark.partitioner.CustomPartitioner;
 import scala.Tuple2;
 
-import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 
-public class PartitionByTest implements Serializable {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PartitionByTest {
     private JavaSparkContext sc;
-    private JavaRDD<String> lines;
-    private JavaPairRDD<String, Iterable<String>> links;
+    private JavaRDD<String> rddA;
+    private JavaPairRDD<String, Iterable<String>> rddB;
 
     @Before
     public void setUp() {
-        sc = new JavaSparkContext(new SparkConf()
-                .setMaster("local[*]")
-                .setAppName("PartitionByTest")
-                .set("spark.driver.allowMultipleContexts", "true"));
-        lines = sc.textFile("src/test/resources/input/PartitionByTest/");
-        lines.foreach(v -> System.out.println(v));
-//        A C
-//        C A
-//        A D
-//        D B
-//        B A
-//        D C
+        sc = new JavaSparkContext("local[*]", "JavaPairRDD.partitionBy");
+        rddA = sc.textFile("src/test/resources/input/PartitionByTest/");
     }
 
     @After
     public void tearDown() {
-        links.glom().foreach(v -> System.out.println(v));
-//        [(A,[C, D]), (C,[A])]
-//        [(B,[A]), (D,[B, C])]
-//
-//        [(A,[C, D])]
-//        [(B,[A])]
-//        [(D,[B, C])]
-//        [(C,[A])]
+        assertEquals(Arrays.asList("A C", "A D", "B A", "C A", "D B", "D C"), rddA.collect());
         sc.close();
     }
 
     @Test
     public void test_without_PartitionBy() {
-        links = lines.mapToPair(line -> {
+        rddB = rddA.mapToPair(line -> {
             String[] parts = line.split(" ");
             return new Tuple2<String, String>(parts[0], parts[1]);
         }).groupByKey();
+        assertEquals(2, rddB.getNumPartitions());
+        assertTrue(customSetAssert(Arrays.asList("[(B,[A]), (D,[B, C])]", "[(A,[C, D]), (C,[A])]"), rddB.glom().collect()));
     }
 
     @Test
     public void test_with_PartitionBy_HashPartitioner() {
-        links = lines.mapToPair(line -> {
+        rddB = rddA.mapToPair(line -> {
             String[] parts = line.split(" ");
             return new Tuple2<String, String>(parts[0], parts[1]);
-        }).groupByKey().partitionBy(new HashPartitioner(4));
+        }).groupByKey().partitionBy(new HashPartitioner(3));
+        assertEquals(3, rddB.getNumPartitions());
+        assertTrue(customSetAssert(Arrays.asList("[(B,[A])]", "[(C,[A])]", "[(D,[B, C]), (A,[C, D])]"), rddB.glom().collect()));
     }
 
     @Test
     public void test_with_PartitionBy_CustomPartitioner() {
-        links = lines.mapToPair(line -> {
+        rddB = rddA.mapToPair(line -> {
             String[] parts = line.split(" ");
             return new Tuple2<String, String>(parts[0], parts[1]);
-        }).groupByKey().partitionBy(new CustomPartitioner(4));
+        }).groupByKey().partitionBy(new CustomPartitioner(3));
+        rddB.foreach(tmp -> System.out.println(tmp));
+        assertEquals(3, rddB.getNumPartitions());
+        assertTrue(customSetAssert(Arrays.asList("[(D,[B, C]), (A,[C, D])]", "[(B,[A])]", "[(C,[A])]"), rddB.glom().collect()));
+    }
+
+    private boolean customSetAssert(List<String> list, List<List<Tuple2<String, Iterable<String>>>> collect) {
+        boolean result = true;
+        String collectStr = collect.toString();
+        for (int i = 0; i < list.size(); i++) {
+            if (!collectStr.contains(list.get(i))) {
+                result = false;
+                break;
+            }
+        }
+        String listStr = list.toString();
+        for (int i = 0; i < collect.size(); i++) {
+            if (!listStr.contains(collect.get(i).toString())) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    static class CustomPartitioner extends Partitioner {
+
+        private int numParts;
+
+        public CustomPartitioner(int numParts) {
+            this.numParts = numParts;
+        }
+
+        @Override
+        public int numPartitions() {
+            return numParts;
+        }
+
+        @Override
+        public int getPartition(Object key) {
+            int code = key.toString().hashCode() % this.numPartitions();
+            if (code < 0) {
+                return code + this.numPartitions();
+            } else {
+                return code;
+            }
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof CustomPartitioner) {
+                CustomPartitioner pObj = (CustomPartitioner) obj;
+                return pObj.numPartitions() == this.numPartitions();
+            } else {
+                return false;
+            }
+        }
     }
 }
