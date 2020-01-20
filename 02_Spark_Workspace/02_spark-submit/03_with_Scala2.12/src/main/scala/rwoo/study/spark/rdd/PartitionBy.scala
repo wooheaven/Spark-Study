727b7bc98fdaaaddae9ae6b91af47c8ee74afe8f
ubuntu <wooheaven79@gmail.com>
2020-01-21 02:22:40 +0900

    #193-scala-partitionby ``` modified:   02_Spark_Workspace/02_spark-submit/03_with_Scala2.12/pom.xml new file:   02_Spark_Workspace/02_spark-submit/03_with_Scala2.12/src/main/scala/rwoo/study/spark/rdd/PartitionBy.scala new file:   03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_PairRDDFunctions/01_Value_Members/01_PairRDDFunctions.partitionBy_example.md renamed:    03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_RDD/01_Concrete_Value_Members/01_RDD.cache_example.md -> 03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/02_class_RDD/01_Concrete_Value_Members/01_RDD.cache_example.md renamed:    03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_RDD/01_Concrete_Value_Members/02_RDD.coalesce_example.md -> 03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/02_class_RDD/01_Concrete_Value_Members/02_RDD.coalesce_example.md renamed:    03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_RDD/01_Concrete_Value_Members/03_RDD.fold_example.md -> 03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/02_class_RDD/01_Concrete_Value_Members/03_RDD.fold_example.md renamed:    03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_RDD/01_Concrete_Value_Members/04_RDD.keyBy_example.md -> 03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/02_class_RDD/01_Concrete_Value_Members/04_RDD.keyBy_example.md renamed:    03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_RDD/01_Concrete_Value_Members/05_RDD.map_example.md -> 03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/02_class_RDD/01_Concrete_Value_Members/05_RDD.map_example.md renamed:    03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_RDD/01_Concrete_Value_Members/06_RDD.take_example.md -> 03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/02_class_RDD/01_Concrete_Value_Members/06_RDD.take_example.md renamed:    03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_RDD/01_Concrete_Value_Members/07_RDD.takeOrdered_example.md -> 03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/02_class_RDD/01_Concrete_Value_Members/07_RDD.takeOrdered_example.md renamed:    03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_RDD/01_Concrete_Value_Members/08_RDD.top_example.md -> 03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/02_class_RDD/01_Concrete_Value_Members/08_RDD.top_example.md new file:   99_Utility/99_modify_README.sh modified:   99_Utility/change_A_to_B.txt modified:   README.md ```

---
+++ 02_Spark_Workspace/02_spark-submit/03_with_Scala2.12/src/main/scala/rwoo/study/spark/rdd/PartitionBy.scala
@@ -0,0 +1,31 @@
+package rwoo.study.spark.rdd
+
+import org.apache.log4j
+import org.apache.log4j.Level
+import org.apache.spark.{SparkContext, HashPartitioner, Partitioner}
+
+object PartitionByTest {
+  val log = log4j.LogManager.getLogger(getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+    log.setLevel(Level.INFO)
+    val sc = new SparkContext("local", "RDD.partitionBy")
+    val rddA = sc.parallelize(Array("James", "Fred", "Anna", "John")).keyBy(x => x.charAt(0)).sortByKey()
+    val rddB = rddA.partitionBy(new HashPartitioner(2))
+    val rddC = rddA.partitionBy(new Partitioner() {
+      override def numPartitions: Int = 2
+      override def getPartition(key: Any): Int = {
+        if (key.asInstanceOf[Char] < 'H') 0 else 1
+      }
+    })
+
+    println(customToString(rddA.glom().collect()))
+    println(customToString(rddB.glom().collect()))
+    println(customToString(rddC.glom().collect()))
+
+    def customToString(a: Array[Array[(Char, String)]]): String = {
+      val str = for (l <- a) yield l.mkString("{", ",", "}")
+      str.mkString("{\n\t", ",\n\t", "\n}")
+    }
+  }
+}
