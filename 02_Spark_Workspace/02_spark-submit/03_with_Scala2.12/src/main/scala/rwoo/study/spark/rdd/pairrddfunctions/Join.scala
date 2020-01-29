77d8f29fc9f343d5189158bdd1aec1188c248326
ubuntu <wooheaven79@gmail.com>
2020-01-29 23:49:32 +0900

    #204-scala-join ``` new file:   02_Spark_Workspace/02_spark-submit/03_with_Scala2.12/src/main/scala/rwoo/study/spark/rdd/pairrddfunctions/Join.scala renamed:    02_Spark_Workspace/02_spark-submit/03_with_Scala2.12/src/main/scala/rwoo/study/spark/rdd/PartitionBy.scala -> 02_Spark_Workspace/02_spark-submit/03_with_Scala2.12/src/main/scala/rwoo/study/spark/rdd/pairrddfunctions/PartitionBy.scala deleted:    03_Spark_Document/04_Pair_RDD_functions_doc/01_for_Scala/03_join_example.md new file:   03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_PairRDDFunctions/01_Value_Members/02_PairRDDFunctios.join_example.md modified:   99_Utility/change_A_to_B.txt modified:   README.md ```

---
+++ 02_Spark_Workspace/02_spark-submit/03_with_Scala2.12/src/main/scala/rwoo/study/spark/rdd/pairrddfunctions/Join.scala
@@ -0,0 +1,35 @@
+package rwoo.study.spark.rdd.pairrddfunctions
+
+import org.apache.log4j
+import org.apache.log4j.Level
+import org.apache.spark.SparkContext
+
+object JoinTest {
+  val log = log4j.LogManager.getLogger(getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+    log.setLevel(Level.INFO)
+    val sc = new SparkContext("local[*]", "RDD.join")
+    val rddA = sc.parallelize(List(("A", 2), ("B", 2)))
+    val rddB = sc.parallelize(List(("B", 3), ("C", 3)))
+
+    println(customToString(rddA.join(rddB).collect()))
+    println(customToString2(rddA.leftOuterJoin(rddB).collect()))
+    println(customToString3(rddA.rightOuterJoin(rddB).collect()))
+
+    def customToString(l: Array[(String, (Int, Int))]): String = {
+      val str = l.mkString("{\n\t", ",\n\t", "\n}")
+      str
+    }
+
+    def customToString2(l: Array[(String, (Int, Option[Int]))]): String = {
+      val str = l.mkString("{\n\t", ",\n\t", "\n}")
+      str
+    }
+
+    def customToString3(l: Array[(String, (Option[Int], Int))]): String = {
+      val str = l.mkString("{\n\t", ",\n\t", "\n}")
+      str
+    }
+  }
+}
---
+++ 03_Spark_Document/07_Scala/01_package_org.apache.spark/01_package_rdd/01_class_PairRDDFunctions/01_Value_Members/02_PairRDDFunctios.join_example.md
@@ -0,0 +1,22 @@
+```
+scala> val rddLeft = sc.parallelize(List(("A",2),("B",2)))
+rddLeft: org.apache.spark.rdd.RDD[(String, Int)] = ParallelCollectionRDD[0] at parallelize at <console>:24
+
+scala> rddLeft.collect()
+res0: Array[(String, Int)] = Array((A,2), (B,2))
+
+scala> val rddRight = sc.parallelize(List(("B",3),("C",3)))
+rddRight: org.apache.spark.rdd.RDD[(String, Int)] = ParallelCollectionRDD[1] at parallelize at <console>:24
+
+scala> rddRight.collect()
+res1: Array[(String, Int)] = Array((B,3), (C,3))
+
+scala> rddLeft.join(rddRight).collect()
+res2: Array[(String, (Int, Int))] = Array((B,(2,3)))
+
+scala> rddLeft.leftOuterJoin(rddRight).collect()
+res3: Array[(String, (Int, Option[Int]))] = Array((B,(2,Some(3))), (A,(2,None)))
+
+scala> rddLeft.rightOuterJoin(rddRight).collect()
+res4: Array[(String, (Option[Int], Int))] = Array((B,(Some(2),3)), (C,(None,3)))
+```
