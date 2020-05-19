63d0a6e1379e1849c728cb0e426851e642568c9e
ubuntu <wooheaven79@gmail.com>
2020-05-19 15:27:03 +0900

    #6-modify-java-coalesce ``` modified:   02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/rdd/CoalesceTest.java modified:   02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/rdd/UnionTest.java ```

--- 02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/rdd/CoalesceTest.java
+++ 02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/rdd/CoalesceTest.java
@@ -6,27 +6,27 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class CoalesceTest {
     private JavaSparkContext sc;
-    private JavaRDD<String> rddA;
-    private JavaRDD<String> rddB;
+    private JavaRDD<Integer> rddA;
+    private JavaRDD<Integer> rddB;
 
     @Before
     public void before() {
         sc = new JavaSparkContext("local[*]", "JavaRDD.coalesce");
-        rddA = sc.parallelize(Arrays.asList("a", "b", "c"), 3);
+        rddA = sc.parallelize(Arrays.asList(1, 2, 3, 2, 3, 10), 3);
     }
 
     @After
     public void after() {
         assertEquals(3, rddA.getNumPartitions());
         assertEquals(2, rddB.getNumPartitions());
+        assertEquals("[[1, 2], [3, 2], [3, 10]]", rddA.glom().collect().toString());
+        assertEquals("[[1, 2], [3, 2, 3, 10]]", rddB.glom().collect().toString());
         sc.close();
     }
 
--- 02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/rdd/UnionTest.java
+++ 02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/rdd/UnionTest.java
@@ -7,7 +7,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,22 +25,10 @@ public class UnionTest {
 
     @After
     public void after() {
-        // prepare assert value
-        List<Integer> array1 = Arrays.asList(1);
-        List<Integer> array23 = Arrays.asList(2, 3);
-        List<List<Integer>> rddAGlom = Arrays.asList(array1, array23);
-        List<Integer> array3 = Arrays.asList(3);
-        List<Integer> array4 = Arrays.asList(4);
-        List<List<Integer>> rddBGlom = Arrays.asList(array3, array4);
-        List<List<Integer>> rddCGlom = Arrays.asList(array1, array23, array3, array4);
-
         // assert
-        assertEquals(Arrays.asList(1, 2, 3), rddA.collect());
-        assertEquals(rddAGlom, rddA.glom().collect());
-        assertEquals(Arrays.asList(3, 4), rddB.collect());
-        assertEquals(rddBGlom, rddB.glom().collect());
-        assertEquals(Arrays.asList(1, 2, 3, 3, 4), rddC.collect());
-        assertEquals(rddCGlom, rddC.glom().collect());
+        assertEquals("[[1], [2, 3]]", rddA.glom().collect().toString());
+        assertEquals("[[3], [4]]", rddB.glom().collect().toString());
+        assertEquals("[[1], [2, 3], [3], [4]]", rddC.glom().collect().toString());
         sc.close();
     }
 
