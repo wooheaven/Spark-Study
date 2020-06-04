c43d83b64584413d735f860c2d7029e5a43f291f
ubuntu <wooheaven79@gmail.com>
2020-06-04 09:54:30 +0900

    #4-spark-operations-max-to-table ``` modified:   02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/javarddlike/MaxTest.java modified:   03_Spark_Document/02_RDD_programming_guide/01_RDD/01_Operations/03_table/01_rdd_table.html ```

--- 02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/javarddlike/MaxTest.java
+++ 02_Spark_Workspace/02_spark-submit/02_with_JDK8/src/test/java/rwoo/study/spark/javarddlike/MaxTest.java
@@ -1,11 +1,11 @@
 package rwoo.study.spark.javarddlike;
 
+import com.google.common.collect.Ordering;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import com.google.common.collect.Ordering;
 
 import java.io.Serializable;
 import java.util.Arrays;
@@ -16,25 +16,27 @@ import static org.junit.Assert.assertEquals;
 public class MaxTest {
     private JavaSparkContext sc;
     private JavaRDD<Integer> rddA;
+    private JavaRDD<String> rddB;
     private Integer max;
-    private Integer maxStr;
+    private String maxStr;
 
     @Before
     public void setUp() {
         sc = new JavaSparkContext("local[*]", "JavaRDDLike.max");
         rddA = sc.parallelize(Arrays.asList(2, 4, 10));
+        rddB = sc.parallelize(Arrays.asList("2", "4", "10"));
     }
 
     @After
     public void after() {
         assertEquals(Arrays.asList(2, 4, 10), rddA.collect());
         assertEquals(10, max, 0.0);
-        assertEquals(4, maxStr, 0.0);
+        assertEquals("4", maxStr);
         sc.close();
     }
 
     @Test
-    public void testMax() {
+    public void testMax_with_Comparator() {
         Object ordering = Ordering.natural();
         final Comparator<Integer> comp = (Comparator<Integer>) ordering;
         max = rddA.max(comp);
@@ -44,21 +46,30 @@ public class MaxTest {
 //                return o1.compareTo(o2);
 //            }
 //        }); org.apache.spark.SparkException: Task not serializable
-
-        ComparatorStr compStr = new ComparatorStr();
-        maxStr = rddA.max(compStr);
-//        maxStr = rddA.max(new Comparator<Integer>() {
-//            @Override
-//            public int compare(Integer o1, Integer o2) {
-//                return o1.toString().compareTo(o2.toString());
-//            }
-//        }); org.apache.spark.SparkException: Task not serializable
+        final Comparator<String> comp2 = (Comparator<String>) ordering;
+        maxStr = rddB.max(comp2);
     }
 
-    static class ComparatorStr implements Comparator<Integer>, Serializable {
+    @Test
+    public void testMax_with_CustomComparator() {
+        CustomComparator customComparator = new CustomComparator();
+        max = rddA.max(customComparator);
+        CustomComparator2 customComparator2 = new CustomComparator2();
+        maxStr = rddB.max(customComparator2);
+    }
+
+    private static class CustomComparator implements Comparator<Integer>, Serializable {
         @Override
         public int compare(Integer o1, Integer o2) {
-            return o1.toString().compareTo(o2.toString());
+            return o1.compareTo(o2);
         }
+    }
+
+    private static class CustomComparator2 implements Comparator<String>, Serializable {
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.compareTo(o2);
+        }
+
     }
 }
